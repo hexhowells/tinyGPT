8156b0d46dde0593a9716393a66a3122b5545556
hexhowells <morganhowells314@gmail.com>
2026-08-03 00:06:20 +0100

    expand download script for multiple datasets

--- scripts/download-fineweb.py
+++
@@ -1,8 +0,0 @@
-from huggingface_hub import snapshot_download
-
-snapshot_download(
-    repo_id="HuggingFaceFW/fineweb",  # or "HuggingFaceFW/fineweb-edu"
-    repo_type="dataset",
-    allow_patterns="sample/10BT/*",
-    local_dir="/media/datasets/fineweb_10BT",
-)
