6842fa35ea007b9f48dadd298326e1a65ff52163
hexhowells <morganhowells314@gmail.com>
2026-08-15 09:19:20 +0100

    update generate to use different models and use cli

--- generate.py
+++ generate.py
@@ -1,38 +1,53 @@
+import argparse
+import time
+
 import torch
 
 from tinyGPT.model import GPT
 from tinyGPT.utils import load_config
 from tinyGPT.bpe import BPETokenizer
 
-import time
+# GPT2: Once upon a time there was a robot
+# GPT2+SFT: <|user|>\nCan you write a python function to check if a number is even or not.\n<|assistant|>\n
 
+parser = argparse.ArgumentParser(prog='TinyGPT', description='Tiny implementation of GPT.')
+parser.add_argument('-m', '--model', type=str, default="gpt2")
+parser.add_argument('-p', '--prompt', type=str)
+parser.add_argument('-s', '--steps', type=int, default=200)
+args = parser.parse_args()
 
 config = load_config()
 
-model = GPT.from_pretrained(model_type=config['model_type'])
+if config['trainer']['device'] == 'auto':
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+else:
+    device = config['trainer']['device']
+
+print(f'Running on device {device}')
+print(f'Using model {args.model}')
+print(f'Generating {args.steps} tokens in total')
+
+model = GPT(config).to(device)
+model_dict = torch.load(f"checkpoints/{args.model}.pt", weights_only=True)
+model.load_state_dict(model_dict['model_state_dict'])
 model.eval()
 
-def generate(prompt='', num_samples=10, steps=20, do_sample=True):
+def generate(prompt, steps, do_sample=True):
     tokenizer = BPETokenizer()
-    if prompt == '':
-        # to create unconditional samples...
-        # manually create a tensor with only the special <|endoftext|> token
-        # similar to what openai's code does here https://github.com/openai/gpt-2/blob/master/src/generate_unconditional_samples.py
-        x = torch.tensor([[tokenizer.encoder.encoder['<|endoftext|>']]], dtype=torch.long)
-    else:
-        x = tokenizer(prompt)
+    x = tokenizer(prompt)
     
-    # we'll process all desired num_samples in a batch, so expand out the batch dim
-    x = x.expand(num_samples, -1)
+    # expand out the batch dim
+    x = x.expand(1, -1)
 
-    # forward the model `steps` times to get samples, in a batch
-    y = model.generate(x, max_new_tokens=steps, do_sample=do_sample, top_k=40)
+    y = model.generate(x.to(device), max_new_tokens=steps, do_sample=do_sample, top_k=40)
     
-    for i in range(num_samples):
-        out = tokenizer.decode(y[i].cpu().squeeze())
-        print('-'*80)
-        print(out)
+    response = tokenizer.decode(y[0].cpu().squeeze())
+    end_of_text = response.find("<|endoftext|>")
+    print("\n" + response[:end_of_text])
 
 start = time.perf_counter()
-generate(prompt="Once upon a time, there was a", num_samples=1, steps=200)
-print(f'Took {(time.perf_counter() - start):.2f} seconds to run.')
+generate(
+    prompt=args.prompt, 
+    steps=args.steps
+)
+print(f'\n[Took {(time.perf_counter() - start):.2f} seconds to run.]')
